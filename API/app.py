48db1e18a2847a4af2f587c8a9d65f7531e3c66b
javrobs <javieroblesamar@gmail.com>
2023-04-27 11:41:59 +0800

    flask api working with populations

--- API/app.py
+++ API/app.py
@@ -1,12 +1,38 @@
 #Import flask and jsonify to run the api
 from flask import Flask,jsonify
+from pymongo import MongoClient
 
-#Define app to run api using Flask
+# Create an instance of MongoClient
+client=MongoClient(port=27017)
+
+# Assign the database to a variable name
+languages=client.languages
+
+# Assign the collections to variables
+populations=languages.populations
+communities=languages.communities
+
+# Define app to run api using Flask
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return 'Hello'
+@app.route("/communities")
+def communities_api():
+    communities_json=communities.find({})
+    communities_list=[]
+    for each in communities_json:
+        each.pop("_id")
+        communities_list.append(each)
+    return jsonify(communities_list)
+
+@app.route("/populations")
+def population_api():
+    population_json=populations.find({})
+    population_list=[]
+    for each in population_json:
+        each.pop("_id")
+        population_list.append(each)
+    return jsonify(population_list)
+
 
         
 #Run app code
