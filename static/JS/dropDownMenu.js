ffff84275c31dd00ea1404c58c80565718ec2f7a
Marijose Cavazos <marijosecavazos@iMac.local>
2023-04-29 22:27:27 -0600

    filtered with dropdown and grabbed subjectLanguage

--- static/JS/dropDownMenu.js
+++ static/JS/dropDownMenu.js
@@ -1,4 +1,5 @@
 // const URL = "http://localhost:5000/populations";
+const URL = "https://data.cityofnewyork.us/resource/ajin-gkbp.json";
 
 function setUp(){
     d3.json(URL).then((data)=>{
@@ -12,7 +13,29 @@ function setUp(){
         console.log(languages);
         languages.forEach(row => {
             d3.select('select').append('option').attr("value" , row).text(row);
-        })
+        });
+        optionChanged(languages[0]);
         });
     }
+
+function optionChanged(subjectLanguage) {
+    console.log(subjectLanguage);
+    demoBox(subjectLanguage);
+    // sunburstFilter(subjectLanguage);
+}
+
+function demoBox(subjectLanguage) {
+    console.log(`demobox for ${subjectLanguage}`);
+    let panel = d3.select('.panel-body');
+    d3.json(URL).then((data) => {
+        panel.html('');
+        panel.append('h6').text(`we're doing a demobox for: ${subjectLanguage}`)
+    // ${key.toUpperCase()}
+    
+
+    //     let panelInfo = data.map
+    //     Object.entries(panelInfo)
+    // })
+});
+}
 setUp();
