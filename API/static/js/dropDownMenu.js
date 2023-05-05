ad676f1b98412b3ba31f7ceea8ce31d6043a2bfb
Marijose Cavazos <marijosecavazos@iMac.local>
2023-05-04 19:03:25 -0600

    solved cors issue

---
+++ API/static/js/dropDownMenu.js
@@ -0,0 +1,36 @@
+const URL = "http://127.0.0.1:5000/populations_all";
+// const URL = "https://data.cityofnewyork.us/resource/ajin-gkbp.json";
+
+function setUp(){
+    d3.json(URL).then((data)=>{
+        // console.log("here is the JSON data!!!")
+        // console.log(data);
+        // fetch languages for dropdown menu
+        let languages=[]
+        data.forEach(row =>  {
+            if (!languages.includes(row.Language)){
+            languages.push(row.Language);
+            }
+        });
+        languages.sort()
+        // console.log(languages);
+        d3.select('select').append('option').attr("value", "All").text("All");
+        languages.forEach(row => {
+            d3.select('select').append('option').attr("value" , row).text(row);
+        });
+        optionChanged("All");
+    });
+}
+
+function optionChanged(subjectLanguage) {
+    console.log(subjectLanguage);
+    demoBox(subjectLanguage);
+    if (subjectLanguage==="All"){
+        allLanguages();
+    }
+    else {
+    sunburstFilter(subjectLanguage);
+    }
+}
+
+setUp();
